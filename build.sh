53e387fb129ebd3fa22db37ee270a390dc2b3eb8
Pete Jemian <prjemian@gmail.com>
2019-07-18 10:30:19 -0500

    MNT #4 w-i-p

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # build the hkl package from source
 
 ####################################################################
-# build these packages in SOURCE_DIR
+# build these packages in BUILD_DIR
 # install them in PREFIX_DIR
 #
 # gtk-doc
@@ -17,17 +17,29 @@ exit 1
 ####################################################################
 # customize for development system
 
-# Mint or el7:
+# "mint-vm" or "wow.aps.anl.gov" or "otz.aps.anl.gov"
+NODE_NAME=`uname -n`
+
+# TODO: ensure Anaconda python is not on the build path
+
+# TODO: determine Mint or RHEL7:
 # (base) mintadmin@mint-vm:/APSshare/linux$ grep "^NAME=" /etc/os-release 
 # NAME="Linux Mint"
 # jemian@wow ~ $ grep "^NAME=" /etc/os-release
 # NAME="Red Hat Enterprise Linux Workstation"
 
-# if []; then
+# if [ Mint ]; then
 #     export APSSHARE=/APSshare/linux
 #     export PREFIX_DIR=${APSSHARE}/64
-#     export SOURCE_DIR=${APSSHARE}/src
-# elif []; then
+#     export BUILD_DIR=${APSSHARE}/src
+#     export GI_DIR_OS=/usr/lib/x86_64-linux-gnu/girepository-1.0
+#     conda deactivate
+# elif [ APS RHEL7 ]; then
+#     export SANDBOX=~/sandbox/build_hkl
+#     export PREFIX_DIR=${SANDBOX}/64
+#     export BUILD_DIR=${SANDBOX}/src
+#     export GI_DIR_OS=/usr/lib/girepository-1.0
+#     export PATH=/home/oxygen/JEMIAN/bin:/usr/local/bin:/bin:/usr/bin:/usr/sbin:/sbin:/usr/lib64/ccache:/usr/local/epics/base/bin/linux-x86_64:/usr/local/epics/base/lib/linux-x86_64:/usr/local/epics/opi/bin/linux-x86_64:/APSshare/epics/extensions-base/3.14.12.3-ext1/bin/linux-x86_64:/APSshare/epics/extensions/bin/linux-x86_64
 # else
 # fi
 
@@ -35,31 +47,36 @@ exit 1
 # configure as needed below
 
 export LDFLAGS=-L${PREFIX_DIR}/lib
+
+# gobject-inspection environment variables
+
+# PKG_CONFIG_PATH needed to find .pc file(s)
 export PKG_CONFIG_PATH=${PREFIX_DIR}/lib/pkgconfig
 
-# GI_TYPELIB_PATH is needed by Python clients to find the typelib file(s)
-GI_TYPELIB_PATH=${PREFIX_DIR}/lib/girepository-1.0
-#???# GI_TYPELIB_PATH=${GI_TYPELIB_PATH}:/usr/lib/girepository-1.0
-export GI_TYPELIB_PATH
+# GI_TYPELIB_PATH needed by Python clients to find .typelib file(s)
+export GI_TYPELIB_PATH=${PREFIX_DIR}/lib/girepository-1.0:${GI_DIR_OS}
 
 ####################################################################
 # download the source code
 
+# TODO: $BUILD_DIR must be an absolute path for this to work
+mkdir -p ${BUILD_DIR}
+
 # gtk-doc
-cd ${SOURCE_DIR}
+cd ${BUILD_DIR}
 git clone https://gitlab.gnome.org/GNOME/gtk-doc.git
 
 # gsl
-cd ${SOURCE_DIR}
+cd ${BUILD_DIR}
 wget http://gnu.mirrors.pair.com/gsl/gsl-latest.tar.gz
 tar xzf gsl-latest.tar.gz
 
 # gobject-introspection
-cd ${SOURCE_DIR}
+cd ${BUILD_DIR}
 git clone https://gitlab.gnome.org/GNOME/gobject-introspection.git
 
 # hkl
-cd ${SOURCE_DIR}
+cd ${BUILD_DIR}
 git clone git://repo.or.cz/hkl.git
 cd hkl
 git checkout -b next origin/next
@@ -68,20 +85,20 @@ git checkout -b next origin/next
 # build the packages
 
 # build gtk-doc
-cd ${SOURCE_DIR}/gtk-doc
+cd ${BUILD_DIR}/gtk-doc
 bash autogen.sh 2>&1 | tee autogen.log
 ./configure --prefix=${PREFIX_DIR} 2>&1 | tee configure.log
 make  2>&1 | tee  make.log
 make install 2>&1 | tee  install.log
 
 # build gsl
-cd ${SOURCE_DIR}/gsl-2.5
+cd ${BUILD_DIR}/gsl-2.5
 ./configure --prefix=${PREFIX_DIR} 2>&1 | tee configure.log
 make  2>&1 | tee  make.log
 make install 2>&1 | tee  install.log
 
 # gobject-introspection
-cd ${SOURCE_DIR}/gobject-introspection
+cd ${BUILD_DIR}/gobject-introspection
 # autogen.sh was removed after this tag
 # Mint: we have glib-2.0 2.56.4
 # 1.60.0 needs glib-2.0 >= 2.58.0
@@ -96,7 +113,7 @@ make  2>&1 | tee  make.log
 make install 2>&1 | tee  install.log
 
 # build hkl
-cd ${SOURCE_DIR}/hkl
+cd ${BUILD_DIR}/hkl
 bash ./autogen.sh 2>&1 | tee autogen.log
 ./configure --prefix=${PREFIX_DIR}  --disable-hkl-doc 2>&1 | tee configure.log
 make  2>&1 | tee  make.log
@@ -106,17 +123,17 @@ make install 2>&1 | tee  install.log
 # reset the packages back to as-downloaded
 
 # reset gtk-doc
-cd ${SOURCE_DIR}/gtk-doc
+cd ${BUILD_DIR}/gtk-doc
 make distclean
 
 # reset gsl
-cd ${SOURCE_DIR}/gsl-2.5
+cd ${BUILD_DIR}/gsl-2.5
 make distclean
 
 # gobject-introspection
-cd ${SOURCE_DIR}/gobject-introspection
+cd ${BUILD_DIR}/gobject-introspection
 make distclean
 
 # reset hkl
-cd ${SOURCE_DIR}/hkl
+cd ${BUILD_DIR}/hkl
 make distclean
