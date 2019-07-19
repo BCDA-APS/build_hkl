7a73c0b70314e3d5a7c3ca08af07d54b3fcb7228
Pete Jemian <prjemian@gmail.com>
2019-07-19 10:09:50 -0500

    MNT #4 WIP add content to build and reset

--- build.sh
+++ build.sh
@@ -25,6 +25,7 @@ BASH=bash
 CD=cd
 DIRNAME=dirname
 ECHO=echo
+EXIT=exit
 GIT=git
 ID=id
 LS=ls
@@ -34,36 +35,85 @@ PS=ps
 READLINK=readlink
 SCRIPT_NAME=${BASH_SOURCE:-$0}
 TAR=tar
+TEE=tee
 WGET=wget
 
 build() {
-    ${ECHO} "${ARG1}"
+    ${ECHO} "Building in ${BUILD_DIR}, installing into ${PREFIX_DIR}"
+
+    # build gtk-doc
+    ${CD} ${BUILD_DIR}/gtk-doc
+    ${BASH} autogen.sh 2>&1 | ${TEE} autogen.log
+    ./configure --prefix=${PREFIX_DIR} 2>&1 | ${TEE} configure.log
+    ${MAKE} 2>&1 | ${TEE}  make.log
+    ${MAKE} install 2>&1 | ${TEE}  install.log
+
+    # build gsl
+    ${CD} ${BUILD_DIR}/gsl-2.5
+    ./configure --prefix=${PREFIX_DIR} 2>&1 | ${TEE} configure.log
+    ${MAKE} 2>&1 | ${TEE}  make.log
+    ${MAKE} install 2>&1 | ${TEE}  install.log
+
+    # gobject-introspection
+    ${CD} ${BUILD_DIR}/gobject-introspection
+    ${BASH} ./autogen.sh 2>&1 | ${TEE} autogen.log
+    ./configure --prefix=${PREFIX_DIR} 2>&1 | ${TEE} configure.log
+    ${MAKE} 2>&1 | ${TEE}  make.log
+    ${MAKE} install 2>&1 | ${TEE}  install.log
+
+    # build hkl
+    ${CD} ${BUILD_DIR}/hkl
+    ${BASH} ./autogen.sh 2>&1 | ${TEE} autogen.log
+    ./configure --prefix=${PREFIX_DIR}  --disable-hkl-doc 2>&1 | ${TEE} configure.log
+    check_gobject_introspection
+    if [ ${check_gobject_introspection_result} != 0 ]; then
+		${ECHO} "ERROR: gobject-introspection package not found during configure step"
+		${ECHO} "  For more details, examine these files:"
+		${ECHO} "    - ${BUILD_DIR}/hkl/configure.log"
+		${ECHO} "    - ${BUILD_DIR}/hkl/config.log"
+		${ECHO} "  Are there conflicting 'gobject-introspection-1.0.pc' files?"
+		${ECHO} "  (Hint: try removing Anaconda Python from PATH)"
+		${EXIT} 1
+    fi
+    ${MAKE} 2>&1 | ${TEE}  make.log
+    ${MAKE} install 2>&1 | ${TEE}  install.log
 }
 
 download() {
-	${MKDIR} -p ${BUILD_DIR}
+    ${MKDIR} -p ${BUILD_DIR}
 
-	${ECHO} "Downloading gtk-doc in ${BUILD_DIR}"
-	${CD} ${BUILD_DIR}
-	${GIT} clone https://gitlab.gnome.org/GNOME/gtk-doc.git
+    ${ECHO} "Downloading gtk-doc in ${BUILD_DIR}"
+    ${CD} ${BUILD_DIR}
+    ${GIT} clone https://gitlab.gnome.org/GNOME/gtk-doc.git
 
-	${ECHO} "Downloading gsl in ${BUILD_DIR}"
-	${CD} ${BUILD_DIR}
-	${WGET} http://gnu.mirrors.pair.com/gsl/gsl-latest.tar.gz
-	${TAR} xzf gsl-latest.tar.gz
+    ${ECHO} "Downloading gsl in ${BUILD_DIR}"
+    ${CD} ${BUILD_DIR}
+    ${WGET} http://gnu.mirrors.pair.com/gsl/gsl-latest.tar.gz
+    ${TAR} xzf gsl-latest.tar.gz
 
-	${ECHO} "Downloading gobject-introspection in ${BUILD_DIR}"
-	${CD} ${BUILD_DIR}
-	${GIT} clone https://gitlab.gnome.org/GNOME/gobject-introspection.git
+    ${ECHO} "Downloading gobject-introspection in ${BUILD_DIR}"
+    ${CD} ${BUILD_DIR}
+    ${GIT} clone https://gitlab.gnome.org/GNOME/gobject-introspection.git
+    ${CD} gobject-introspection
+    # autogen.sh was removed after this tag
+    # Mint: we have glib-2.0 2.56.4
+    # 1.60.0 needs glib-2.0 >= 2.58.0
+    # 1.59.4 needs glib-2.0 >= 2.58.0
+    # 1.57.2 needs glib-2.0 >= 2.57.2
+    # 1.56.1 needs glib-2.0 >= 2.56.1
+    # 1.56.0 needs glib-2.0 >= 2.56.0
+    ${ECHO} "git checkout tag 1.56.1 of gobject-introspection"
+    ${GIT} checkout 1.56.1
 
-	${ECHO} "Downloading hkl in ${BUILD_DIR}"
-	${CD} ${BUILD_DIR}
-	${GIT} clone git://repo.or.cz/hkl.git
-	${CD} hkl
-	${GIT} checkout -b next origin/next
-	
-	${ECHO} "Directory of ${BUILD_DIR}"
-	${LS} -lAFgh ${BUILD_DIR}
+    ${ECHO} "Downloading hkl in ${BUILD_DIR}"
+    ${CD} ${BUILD_DIR}
+    ${GIT} clone git://repo.or.cz/hkl.git
+    ${CD} hkl
+    ${ECHO} "git checkout branch 'next' of gobject-introspection"
+    ${GIT} checkout -b next origin/next
+    
+    ${ECHO} "Directory of ${BUILD_DIR}"
+    ${LS} -lAFgh ${BUILD_DIR}
 }
 
 info() {
@@ -93,110 +143,137 @@ info() {
     ${ECHO} "WGET:            ${WGET}"
 }
 
-setup() {
-	IS_MINT=`grep "^NAME=" /etc/os-release | grep "Linux Mint"`
-	IS_RHEL=`grep "^NAME=" /etc/os-release | grep "Red Hat Enterprise Linux"`
-	if [ "" != "${IS_MINT}" ] ; then
-		export OS=Mint
-		export GI_DIR_OS=/usr/lib/x86_64-linux-gnu/girepository-1.0
-	elif [ "" != "${IS_RHEL}" ] ; then
-		export OS=Mint
-		export GI_DIR_OS=/usr/lib/girepository-1.0
-	else
-		export OS=
-	fi
-	
-	# only if ${PREFIX_DIR}/bin not already in path
-	_part=${PREFIX_DIR}/bin
-	append_or_define_path ${PATH} ${_part}
-	export PATH=${append_or_define_path_result}
-	# echo $PATH
-	
-	if [ -z "$IOC_STARTUP_DIR" ] ; then
-		export LD_LIBRARY_PATH=${PREFIX_DIR}/lib
-	else
-		export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PREFIX_DIR}/lib
-	fi
-	export LDFLAGS=-L${PREFIX_DIR}/lib
-	
-	# for gobject-introspection
-	# PKG_CONFIG_PATH needed to find .pc file(s)
-	export PKG_CONFIG_PATH=${PREFIX_DIR}/lib/pkgconfig
+reset() {
+    ${ECHO} "resetting source directories to distclean state"
 
-	# GI_TYPELIB_PATH needed by Python clients to find .typelib file(s)
-	export GI_TYPELIB_PATH=${PREFIX_DIR}/lib/girepository-1.0:${GI_DIR_OS}
+    # reset gtk-doc
+    ${CD} ${BUILD_DIR}/gtk-doc
+    ${MAKE} distclean
+
+    # reset gsl
+    ${CD} ${BUILD_DIR}/gsl-2.5
+    ${MAKE} distclean
+
+    # gobject-introspection
+    ${CD} ${BUILD_DIR}/gobject-introspection
+    ${MAKE} distclean
+
+    # reset hkl
+    ${CD} ${BUILD_DIR}/hkl
+    ${MAKE} distclean
+}
+
+setup() {
+    IS_MINT=`grep "^NAME=" /etc/os-release | grep "Linux Mint"`
+    IS_RHEL=`grep "^NAME=" /etc/os-release | grep "Red Hat Enterprise Linux"`
+    if [ "" != "${IS_MINT}" ] ; then
+        export OS=Mint
+        export GI_DIR_OS=/usr/lib/x86_64-linux-gnu/girepository-1.0
+    elif [ "" != "${IS_RHEL}" ] ; then
+        export OS=Mint
+        export GI_DIR_OS=/usr/lib/girepository-1.0
+    else
+        export OS=
+    fi
+    
+    # only if ${PREFIX_DIR}/bin not already in path
+    _part=${PREFIX_DIR}/bin
+    append_or_define_path ${PATH} ${_part}
+    export PATH=${append_or_define_path_result}
+    # echo $PATH
+    
+    if [ -z "$IOC_STARTUP_DIR" ] ; then
+        export LD_LIBRARY_PATH=${PREFIX_DIR}/lib
+    else
+        export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PREFIX_DIR}/lib
+    fi
+    export LDFLAGS=-L${PREFIX_DIR}/lib
+    
+    # for gobject-introspection
+    # PKG_CONFIG_PATH needed to find .pc file(s)
+    export PKG_CONFIG_PATH=${PREFIX_DIR}/lib/pkgconfig
+
+    # GI_TYPELIB_PATH needed by Python clients to find .typelib file(s)
+    export GI_TYPELIB_PATH=${PREFIX_DIR}/lib/girepository-1.0:${GI_DIR_OS}
 }
 
 ####################################################################
 
+check_gobject_introspection() {
+	# set result to:
+	#    0 if no problem
+	#    1 if problem
+
+	# Problem occurs when we try to find gobject-introspection
+	# The system provides one version, Anaconda another
+	# Detect this *after* the configure step during the build.
+	# if problem: configure.log contains this message: 
+	#    checking for gobject-introspection... no
+	# if no problem: configure.log contains this message: 
+	#    checking for gobject-introspection... yes
+	check_gobject_introspection_result=1
+}
+
 # TODO: replace with better
 contains() {
-	_source=$1
-	_item=$2
-	contains_result=0
-	IFS=':'
-	for _s in $_source; do
-		if [ "$_item" == "$_s" ] ; then
-			contains_result=${contains_result} + 1
-		fi
-	done
-	unset IFS
-	unset _source
-	unset _item
+    _source=$1
+    _item=$2
+    contains_result=0
+    IFS=':'
+    for _s in $_source; do
+        if [ "$_item" == "$_s" ] ; then
+            contains_result=${contains_result} + 1
+        fi
+    done
+    unset IFS
+    unset _source
+    unset _item
 }
 
 # TODO: replace with better
 append_or_define_path() {
-	target=$1
-	entry=$2
-	if [ "" == "${target}" ] ; then
-		append_or_define_path_result=${entry}
-	elif [ "" == "${entry}" ] ; then
-		append_or_define_path_result=${target}
-	else
-		append_or_define_path_result=${target}:${entry}
-	fi
-	# echo "append_or_define_path_result: $append_or_define_path_result"
+    target=$1
+    entry=$2
+    if [ "" == "${target}" ] ; then
+        append_or_define_path_result=${entry}
+    elif [ "" == "${entry}" ] ; then
+        append_or_define_path_result=${target}
+    else
+        append_or_define_path_result=${target}:${entry}
+    fi
+    # echo "append_or_define_path_result: $append_or_define_path_result"
 }
 
-# FIXME: stops appending after two items, replace with better
-strip_item_from_path() {
-	_item=$1
-	IFS=':'
-	_new_path=
-	for _s in $PATH; do
-		#echo "examining: $_s"
-		if [[ "$_s" != *"$_item"* ]] ; then
-			append_or_define_path ${_new_path} ${_s}
-			_new_path=${append_or_define_path_result}
-		fi
-		# echo "---------------"
-	done
-	unset IFS
-	#echo "_new_path : $_new_path"
-}
-
-tests() {
+developer() {
     #${ECHO} "${ARG1}"
-	#if [ "" == "${IOC_PID}" ] ; then
-	#  echo "IOC_PID matches empty string"
-	#fi
-	#if [ -z "$IOC_STARTUP_DIR" ] ; then
-	#  echo "-z was True : IOC_STARTUP_DIR='$IOC_STARTUP_DIR'"
-	#fi
-	#if [ -d "/usr/lib/x86_64-linux-gnu/" ] ; then
-	#  echo "/usr/lib/x86_64-linux-gnu/: yes"
-	#fi
-	#if [ -d "/no/such/path/" ] ; then
-	#  echo "/no/such/path/: yes"
-	#fi
+    #if [ "" == "${IOC_PID}" ] ; then
+    #  echo "IOC_PID matches empty string"
+    #fi
+    #if [ -z "$IOC_STARTUP_DIR" ] ; then
+    #  echo "-z was True : IOC_STARTUP_DIR='$IOC_STARTUP_DIR'"
+    #fi
+    #if [ -d "/usr/lib/x86_64-linux-gnu/" ] ; then
+    #  echo "/usr/lib/x86_64-linux-gnu/: yes"
+    #fi
+    #if [ -d "/no/such/path/" ] ; then
+    #  echo "/no/such/path/: yes"
+    #fi
 
-	strip_item_from_path anaconda/
+    check_gobject_introspection
+    if [ ${check_gobject_introspection_result} != 0 ]; then
+		${ECHO} "ERROR: gobject-introspection package not found during configure step"
+		${ECHO} "  For more details, examine these files:"
+		${ECHO} "    - ${BUILD_DIR}/hkl/configure.log"
+		${ECHO} "    - ${BUILD_DIR}/hkl/config.log"
+		${ECHO} "  Are there conflicting 'gobject-introspection-1.0.pc' files?"
+		${ECHO} "  (Hint: try removing Anaconda Python from PATH)"
+		${EXIT} 1
+    fi
 
 }
 
 usage() {
-    ${ECHO} "Usage: $(${BASENAME} ${SCRIPT_NAME}) {build|download|info|reset|tests}"
+    ${ECHO} "Usage: $(${BASENAME} ${SCRIPT_NAME}) {build|download|info|reset}"
 }
 
 ####################################################################
@@ -217,9 +294,9 @@ case ${ARG1} in
     info
     ;;
 
-    tests)
+    developer)
     setup
-    tests
+    developer
     ;;
 
     reset)
